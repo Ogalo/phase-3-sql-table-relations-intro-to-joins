26ebdbd5c6308886c62c458341da5100a0802ae6
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-31 03:59:31 +0300

    Done

--- test.sql
+++ test.sql
@@ -6,7 +6,35 @@
 -- ON cats.owner_id = owner_id;
 
 
-SELECT cats.name, cats.breed, owners.name
+-- SELECT cats.name, cats.breed, owners.name
+-- FROM cats
+-- LEFT OUTER JOIN owners
+-- ON cats.owner_id = owners.id;
+--  SELECT * FROM cats WHERE owner_id = 1;
+
+CREATE TABLE cat_owners (
+  cat_id INTEGER,
+  owner_id INTEGER
+);
+
+INSERT INTO cat_owners (cat_id, owner_id)
+VALUES (3, 2), (3, 3);
+SELECT * FROM cat_owners;
+
+ SELECT cat_owners.owner_id
+ FROM cat_owners
+ WHERE cat_id = 3;
+
+ SELECT owners.name
+FROM owners
+INNER JOIN cat_owners
+ON owners.id = cat_owners.owner_id WHERE cat_owners.cat_id = 3;
+
+SELECT
+  cats.name AS cat_name,
+  owners.name AS owner_name
 FROM cats
-LEFT OUTER JOIN owners
-ON cats.owner_id = owners.id;
+INNER JOIN cat_owners
+  ON cats.id = cat_owners.cat_id
+INNER JOIN owners
+  ON cat_owners.owner_id = owners.id;
